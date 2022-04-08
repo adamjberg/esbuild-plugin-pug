70fddf74be90fb24c3351e427e6911d48bc543ec
Adam Berg <adam@xyzdigital.com>
2022-04-08 14:54:46 -0400

    Add filename to pug compile

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import fs from 'fs';
-import pug from "pug";
+const fs = require("fs");
+const pug = require("pug");
 
 const pluginPug = () => ({
-	name: 'pug',
-	setup(build) {
-		build.onLoad({ filter: /\.jade$/ }, async args => {
-			let template = await fs.promises.readFile(args.path, 'utf8');
+  name: "pug",
+  setup(build) {
+    build.onLoad({ filter: /\.jade$/ }, async (args) => {
+      let template = await fs.promises.readFile(args.path, "utf8");
 
-      const contents = pug.compile(template)()
+      const contents = pug.compile(template, { filename: args.path })();
 
-			return { contents, loader: "text" };
-		});
-	}
+      return { contents, loader: "text" };
+    });
+  },
 });
 
-export default pluginPug;
+module.exports = pluginPug;
